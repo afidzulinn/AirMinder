0fd5bd806fccd5e893b5e0887005e3029b36dd24
Afidz Ulinn <afidzulin89@gmail.com>
2023-09-06 16:40:14 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+app = Flask(__name__, template_folder="templates", static_folder="static")
+
+# loaded Model Machine Learning
+model = joblib.load('model/dt.sav')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        # Get the input values from the form
+        pm10 = request.form.get("pm10")
+        so2 = request.form.get("so2")
+        co = request.form.get("co")
+        o3 = request.form.get("o3")
+        no2 = request.form.get("no2")
+        
+        if not (pm10 and so2 and co and o3 and no2):
+            return render_template("index.html", error="Please fill in all the fields.")
+        
+        try:
+            pm10 = float(pm10)
+            so2 = float(so2)
+            co = float(co)
+            o3 = float(o3)
+            no2 = float(no2)
+        except ValueError:
+            return render_template("index.html", error="Please enter valid numeric values.")
+        
+        input_arr = np.array([pm10, so2, co, o3, no2]).reshape(1, -1)
+        
+        prediction = model.predict(input_arr)
+        output = prediction[0]
+        
+        # Baik
+        if output == 0:
+            return render_template("baik.html")
+        # Sangat tidak sehat
+        elif output == 1:
+            return render_template("sangat tidak sehat.html")
+        # Sedang
+        elif output == 2:
+            return render_template("sedang.html")
+        # Tidak sehat
+        else:
+            return render_template("tidak sehat.html")
+    
+    return render_template('predict.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/assets1/js/main.js
@@ -0,0 +1,117 @@
+/*
+	Alpha by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$header = $('#header'),
+		$banner = $('#banner');
+
+	// Breakpoints.
+		breakpoints({
+			wide:      ( '1281px',  '1680px' ),
+			normal:    ( '981px',   '1280px' ),
+			narrow:    ( '737px',   '980px'  ),
+			narrower:  ( '737px',   '840px'  ),
+			mobile:    ( '481px',   '736px'  ),
+			mobilep:   ( null,      '480px'  )
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			alignment: 'right'
+		});
+
+	// NavPanel.
+
+		// Button.
+			$(
+				'<div id="navButton">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+
+	// Header.
+		if (!browser.mobile
+		&&	$header.hasClass('alt')
+		&&	$banner.length > 0) {
+
+			$window.on('load', function() {
+
+				$banner.scrollex({
+					bottom:		$header.outerHeight(),
+					terminate:	function() { $header.removeClass('alt'); },
+					enter:		function() { $header.addClass('alt reveal'); },
+					leave:		function() { $header.removeClass('alt'); }
+				});
+
+			});
+
+		}
+
+		$('#fileup').change(function(){
+			//here we take the file extension and set an array of valid extensions
+				var res=$('#fileup').val();
+				var arr = res.split("\\");
+				var filename=arr.slice(-1)[0];
+				filextension=filename.split(".");
+				filext="."+filextension.slice(-1)[0];
+				valid=[".jpg",".png",".jpeg",".bmp"];
+			//if file is not valid we show the error icon, the red alert, and hide the submit button
+				if (valid.indexOf(filext.toLowerCase())==-1){
+					$( ".imgupload" ).hide("slow");
+					$( ".imgupload.ok" ).hide("slow");
+					$( ".imgupload.stop" ).show("slow");
+				  
+					$('#namefile').css({"color":"red","font-weight":700});
+					$('#namefile').html("File "+filename+" is not  pic!");
+					
+					$( "#submitbtn" ).hide();
+					$( "#fakebtn" ).show();
+				}else{
+					//if file is valid we show the green alert and show the valid submit
+					$( ".imgupload" ).hide("slow");
+					$( ".imgupload.stop" ).hide("slow");
+					$( ".imgupload.ok" ).show("slow");
+				  
+					$('#namefile').css({"color":"green","font-weight":700});
+					$('#namefile').html(filename);
+				  
+					$( "#submitbtn" ).show();
+					$( "#fakebtn" ).hide();
+				}
+			});
+
+})(jQuery);
