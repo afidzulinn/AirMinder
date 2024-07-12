147d743efd7a1a491e8337c28a20083dce078f21
Afidz Ulinnuha <97087553+afidzulinn@users.noreply.github.com>
2024-07-12 09:23:31 +0700

    change port

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def predict():
     return render_template('predict.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port=9090)
+    app.run(host="0.0.0.0", debug=True, port=9123)
